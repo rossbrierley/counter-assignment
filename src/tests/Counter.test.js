4fa8c87b167e8c1b4964f56af8d688ebe2fa8ca2
rossbrierley <115773271+rossbrierley@users.noreply.github.com>
2024-11-12 17:01:24 -0500

    Automated unit tests complete in Counter.test.js.

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,40 +1,39 @@
-// import necessary react testing library helpers here
-// import the Counter component here
-import {render, screen, fireEvent} from '@testing-library/react'
-import '@testing-library/jest-dom'
-import Counter from '../components/Counter'
+import React from 'react';
+import {render, screen, fireEvent} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
-  // Render the Counter component here
   render(<Counter />);
 });
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
-  counterMessage = screen.getByText(/Counter/i);
+  const counterMessage = screen.getByText(/Counter/i);
   expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
-  countElement = screen.getByText(/0/);
-  expect(countElement).toBeInTheDocument();
+  const countElement = screen.getByTestId('count');
+  expect(countElement).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
-  incrementButton = screen.getByText('+');
+  const incrementButton = screen.getByText('+');
   fireEvent.click(incrementButton);
-  countElement = screen.getByText(/1/);
-  expect(countElement).toBeInTheDocument();
+  const countElement = screen.getByTestId('count');
+  expect(countElement).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
-  decrementButton = screen.getByText('-');
-  incrementButton = screen.getByText('+');
+  const decrementButton = screen.getByText('-');
+  const incrementButton = screen.getByText('+');
+
+  // Increment first to make sure we don't go negative initially
   fireEvent.click(incrementButton);
+
+  // Decrement the count back to 0
   fireEvent.click(decrementButton);
-  const countElement = screen.getByText(/0/);
-  expect(countElement).toBeInTheDocument();
+
+  const countElement = screen.getByTestId('count');
+  expect(countElement).toHaveTextContent('0');
 });
