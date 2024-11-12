e6c24cd7ec3076b2d2062bc480cf24ea55885d18
rossbrierley <115773271+rossbrierley@users.noreply.github.com>
2024-11-12 16:19:26 -0500

    Unit tests functions implemented. Testing with npm test scripts.

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,40 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import {render, screen, fireEvent} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Counter from '../components/Counter'
 
 beforeEach(() => {
   // Render the Counter component here
-})
+  render(<Counter />);
+});
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  countElement = screen.getByText(/0/);
+  expect(countElement).toBeInTheDocument();
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  incrementButton = screen.getByText('+');
+  fireEvent.click(incrementButton);
+  countElement = screen.getByText(/1/);
+  expect(countElement).toBeInTheDocument();
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  decrementButton = screen.getByText('-');
+  incrementButton = screen.getByText('+');
+  fireEvent.click(incrementButton);
+  fireEvent.click(decrementButton);
+  const countElement = screen.getByText(/0/);
+  expect(countElement).toBeInTheDocument();
 });
